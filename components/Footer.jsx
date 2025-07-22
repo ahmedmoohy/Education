ab06cd212dbc9049acad689f748587991c2f955f
ahmedmoohy <ahmedmohyenglevel1@gmail.com>
2025-07-22 13:27:01 +0300

    Create Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-white shadow p-4 mt-8 text-center">
+      <p>© 2025 EduPlatform. All rights reserved.</p>
+    </footer>
+  );
+}
